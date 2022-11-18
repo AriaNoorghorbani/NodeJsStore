ebf4a0cf6d8ec5b69d07ff3f04e462d83762756f
arianoorqorbani <aria.noorghorbani@gmail.com>
2022-11-18 17:19:21 +0300

    add ejs to html

--- routes/admin.js
+++ routes/admin.js
@@ -5,16 +5,13 @@ const path = require('path')
 const products = []
 
 router.get('/add-product', (req, res, next) => {
-    // res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'))
     res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' })
 });
 
 router.post('/add-product', (req, res, next) => {
-    products.push(req.body.title)
-    // console.log(req.body.title);
+    products.push({ title: req.body.title })
     res.redirect('/')
 })
 
-// module.exports = router
 exports.routes = router;
 exports.products = products
