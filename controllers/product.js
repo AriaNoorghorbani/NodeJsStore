89cb44538fc0eb01d753a93b0c5f26914dccfe76
arianoorqorbani <aria.noorghorbani@gmail.com>
2022-11-18 19:16:46 +0300

    add controller

---
+++ controllers/product.js
@@ -0,0 +1,21 @@
+const products = []
+
+exports.getProducts = (req, res, next) => {
+    res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' })
+}
+
+exports.postProducts = (req, res, next) => {
+    products.push({ title: req.body.title })
+    res.redirect('/')
+}
+
+exports.shop = (req, res, next) => {
+    res.render('shop', {
+        prods: products,
+        pageTitle: 'My Shop',
+        path: '/',
+        hasProduct: products.length > 0,
+        productCss: true,
+        activeShop: true
+    });
+}
--- routes/admin.js
+++ routes/admin.js
@@ -1,17 +1,10 @@
 const express = require('express')
 const router = express.Router()
+const controllers = require('../controllers/product')
 const path = require('path')
 
-const products = []
+router.get('/add-product', controllers.getProducts);
 
-router.get('/add-product', (req, res, next) => {
-    res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' })
-});
+router.post('/add-product', controllers.postProducts)
 
-router.post('/add-product', (req, res, next) => {
-    products.push({ title: req.body.title })
-    res.redirect('/')
-})
-
-exports.routes = router;
-exports.products = products
+module.exports = router
