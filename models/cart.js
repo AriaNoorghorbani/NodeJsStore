8aa96796b075357fa06c063b76154b7b600e01d3
arianoorqorbani <aria.noorghorbani@gmail.com>
2022-11-24 17:58:46 +0300

    .

--- models/cart.js
+++ models/cart.js
@@ -4,7 +4,7 @@ const path = require('path')
 const p = path.join(path.dirname(require.main.filename), 'data', 'cart.json')
 
 exports.module = class Cart {
-    static addProduct(id) {
+    static addProduct(id, productPrice) {
         // Fetch the previous cart 
         fs.readFile(p, (err, fileContent) => {
             let cart = { product: [], totalPrice: 0 }
@@ -14,12 +14,14 @@ exports.module = class Cart {
             // Analyze the cart => Find existing product
             const existingProduct = cart.product.find(prod => prod.id === id)
             let updatedProduct
+            // Add new product/ increase quantity
             if (existingProduct) {
                 updatedProduct = { ...existingProduct }
                 updatedProduct.qty = updatedProduct.qty + 1
             } else {
                 updatedProduct = { id: id, qty: 1 }
             }
+            cart.totalPrice = cart.totalPrice + productPrice
         })
     }
 }
