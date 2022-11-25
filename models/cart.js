fda1877de718e4a73030faf336aa691ee03e6f96
arianoorqorbani <aria.noorghorbani@gmail.com>
2022-11-25 07:36:09 +0300

    .

--- models/cart.js
+++ models/cart.js
@@ -12,16 +12,23 @@ exports.module = class Cart {
                 cart = JSON.parse(fileContent)
             }
             // Analyze the cart => Find existing product
-            const existingProduct = cart.product.find(prod => prod.id === id)
+            const existingProductIndex = cart.product.findIndex(prod => prod.id === id)
+            const existingProduct = cart.product[existingProductIndex]
             let updatedProduct
             // Add new product/ increase quantity
             if (existingProduct) {
                 updatedProduct = { ...existingProduct }
                 updatedProduct.qty = updatedProduct.qty + 1
+                cart.product = [...cart.product]
+                cart.product[existingProductIndex] = updatedProduct
             } else {
                 updatedProduct = { id: id, qty: 1 }
+                cart.product = [...cart.product, updatedProduct]
             }
             cart.totalPrice = cart.totalPrice + productPrice
+        })
+        fs.writeFile(p, JSON.stringify(cart), err => {
+            console.log(err);
         })
     }
 }
